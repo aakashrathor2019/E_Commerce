4f122a1a219ac16877ca2ca0387875c2c7dac628
Aakash_Rathor <154987650+aakashrathor2019@users.noreply.github.com>
2025-04-16 11:11:18 +0530

    Update urls.py

--- shopping/urls.py
+++ shopping/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from  . views import *
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth.decorators import login_required
 
 
 
